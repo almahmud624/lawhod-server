b7bf287f7f8acf1ed7f275bf0b0d0d40d8bfbe22
almahmud624 <mahmudbhuiyan624@gmail.com>
2022-11-10 15:54:00 +0600

    jwt authetication added

--- index.js
+++ index.js
@@ -1,12 +1,31 @@
 const express = require("express");
 const cors = require("cors");
 const { MongoClient, ServerApiVersion } = require("mongodb");
+// require jwt
+const jwt = require("jsonwebtoken");
 const ObjectId = require("mongodb").ObjectId;
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 4000;
 app.use(cors());
 app.use(express.json());
+
+// JWT verify function
+const verifyJWT = (req, res, next) => {
+  const jwtHead = req.headers.authorization;
+
+  if (!jwtHead) {
+    return res.status(401).send({ message: "Unauthorized Access" });
+  }
+  const token = jwtHead.split(" ")[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(403).send("Forbidden Access");
+    }
+    req.decoded = decoded; // decode the code
+    next();
+  });
+};
 
 const uri = process.env.DB_URI;
 const client = new MongoClient(uri, {
@@ -19,6 +38,15 @@ async function run() {
   try {
     const practiceCollection = client.db("law-hod").collection("practiceAreas");
     const reviewCollection = client.db("law-hod").collection("reviews");
+
+    // jwt
+    app.post("/jwt", (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1d",
+      });
+      res.send({ token });
+    });
 
     // send practice area on server
     app.post("/practice-areas", async (req, res) => {
@@ -34,7 +62,7 @@ async function run() {
       if (size === 3) {
         sorted = { _id: -1 };
       } else {
-        sorted = { _id: 0 };
+        sorted = {};
       }
       const practiceAreas = await practiceCollection
         .find({})
@@ -63,12 +91,27 @@ async function run() {
     // get review data from server
     app.get("/reviews", async (req, res) => {
       let query = {};
-      if (req.query.email) {
+      if (req.query.practiceId) {
         query = {
-          "reviewersInfo.email": req.query.email,
+          practiceId: req.query.practiceId,
         };
       }
       res.send(await reviewCollection.find(query).toArray());
+    });
+
+    // get reviews by practices email
+    app.get("/reviews/:email", verifyJWT, async (req, res) => {
+      // valid email check for JWT
+      const decode = req.decoded;
+      if (decode?.email !== req.params?.email) {
+        return res.status(403).send("Access Forbiden");
+      }
+      const email = req.params.email;
+      const query = {
+        "reviewersInfo.email": email,
+      };
+      const result = await reviewCollection.find(query).toArray();
+      res.send(result);
     });
 
     // delete remove
